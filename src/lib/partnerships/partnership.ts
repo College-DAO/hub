56454ec740e28e7f897f61a0229c47ef7d01bbc2
Anshul Dalua <adalua@umich.edu>
2024-06-22 17:35:18 -0400

    added editPartnership funcitonality
    need to connect to backend + view partnership, need to fix what info being sent to backend too

--- src/lib/partnerships/partnership.ts
+++ src/lib/partnerships/partnership.ts
@@ -1,18 +1,21 @@
 interface Partnership {
-    id: any; // Replace 'any' with a more specific type if possible, e.g., 'number' or 'string'
-    sender_id: any; // Same here
-    receiver_id: any; // And here
-    type: any; // Replace 'any' with the correct type, e.g., 'string'
-    duration: any;
-    funding: any
-    status: any
-    sender_name: any
-    partner_name: any
-    details: any
-    // Adjust the type as necessary
-    // Add any other fields that might be included in your partnership records
-  }
+  id: string; // Replace with the specific type if possible, e.g., 'number' or 'string'
+  sender_id: string; // Replace with the correct type, e.g., 'number' or 'string'
+  receiver_id: string; // Replace with the correct type, e.g., 'number' or 'string'
+  type: string; // Replace with the correct type, e.g., 'string'
+  partnershipName: string;
+  durationStart: string;
+  durationEnd: string;
+  funding: number;
+  status: string;
+  sender_name: string;
+  partner_name: string;
+  recepient_name: string;
+  details: string;
+  format: string; // Assuming you missed this in the interface
+  name: string; // Assuming you missed this in the interface
+  kpis: Kpi[]; // Adjust the type as necessary
+  // Add any other fields that might be included in your partnership records
+}
 
-  
 export default Partnership;
-  
