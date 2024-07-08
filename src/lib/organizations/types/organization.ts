4ca71a769c62435866801e14a96b996acafa8466
Joshua Sum <joshsum@umich.edu>
2024-07-07 23:53:40 -0400

    Fix organizations logo update bug

--- src/lib/organizations/types/organization.ts
+++ src/lib/organizations/types/organization.ts
@@ -1,11 +1,11 @@
 interface Organization {
   id: number;
-  uuid: string;
-  name: string;
-  email: string;
-  type: string;
-  approved: boolean;
-  socials: Object;
+  uuid?: string;
+  name?: string;
+  email?: string;
+  type?: string;
+  approved?: boolean;
+  socials?: Object;
   logoURL?: string | null;
 }
 
