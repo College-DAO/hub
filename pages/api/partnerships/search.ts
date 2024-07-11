c125467b0bc376dd4a68736d791c5205808ff105
Anshul Dalua <adalua@umich.edu>
2024-07-10 23:51:42 -0400

    update

--- pages/api/partnerships/search.ts
+++ pages/api/partnerships/search.ts
@@ -11,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { query } = req.query;
     
     const { data, error } = await supabase
-      .from('organizations_duplicate')
+      .from('organizations')
       .select('*')
       .ilike('name', `%${query}%`);
     
